b16f8d5094e0a1d923d4cd7254b3e1402db7d01e
pengxuemin <903322147@qq.com>
2025-03-10 11:16:19 +0800

    fix:createWebHistory添加入参

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { loadFinish } from '@/utils/app';
 
 // @ts-ignore
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.VITE_PUBLIC_PATH || '/'),
     routes: [
         {
             path: '/',
